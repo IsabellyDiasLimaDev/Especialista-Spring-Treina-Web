1628ce8c0172b1c60c8e02bfc0107687bba0a424
IsabellyDiasLimaDev <isabellydias29@gmail.com>
2024-06-15 15:48:02 -0300

    Projeto Inicial - aula 4 do curso spring boot fundamentos

---
+++ src/main/java/br/com/treinaweb/estudos_spring/EstudosSpringNaTreinaWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.estudos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosSpringNaTreinaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosSpringNaTreinaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinaweb/estudos_spring/EstudosSpringNaTreinaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.estudos_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringNaTreinaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
